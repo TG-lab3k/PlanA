41054f16d6f33bd62290006ec1447184c5ec9d44
leiguoting <578177501@qq.com>
2014-07-09 14:27:13 +0800

    init android project

---
+++ Android/src/com/cubieline/plana/PaAppliction.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.cubieline.plana;
+
+import android.app.Application;
+
+/**
+ * @author LeiGuoting
+ *
+ */
+public class PaAppliction extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
